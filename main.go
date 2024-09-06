cae1dbf632e5fc26c08d8a692715c1c3c9f776ec
imad-07 <tounsadiimad@gmail.com>
2024-09-06 09:56:31 +0100

    heyyy

---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+	"sync"
+)
+
+var (
+	clients = make(map[net.Conn]string) // Map of connected clients
+	mu      sync.Mutex                  // Mutex to protect the clients map
+)
+
+func handleConnection(conn net.Conn) {
+	text, err1 := os.ReadFile("t.txt")
+	if err1 != nil {
+		return
+	}
+
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+	name := ""
+	conn.Write([]byte("Welcome to TCP-Chat!\n"))
+	conn.Write(text)
+
+	var err error
+	count := 0
+	for len(name) == 0 {
+		conn.Write([]byte("[ENTER YOUR NAME]:"))
+		name, err = reader.ReadString('\n')
+		name = strings.TrimSpace(name)
+		if err != nil || len(name) == 0 {
+			conn.Write([]byte("Error reading name\n"))
+			fmt.Println("Error reading name")
+			count++
+
+		}
+		if count == 3 {
+			conn.Write([]byte("you ran out of tries\n"))
+			return
+		}
+
+	}
+
+	fmt.Println("Client connected with name:", name)
+
+	mu.Lock()
+	clients[conn] = name // Add client to the map
+	mu.Unlock()
+
+	// Send welcome message
+	conn.Write([]byte("Welcome, " + name + "!\n"))
+	for {
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Client disconnected:", err)
+
+			// Remove client from the list when disconnected
+			mu.Lock()
+			delete(clients, conn)
+			mu.Unlock()
+
+			break
+		}
+
+		message = strings.TrimSpace(message)
+
+		fmt.Println(name + ": " + message)
+		if len(message) != 0 {
+			// Broadcast the message to all clients
+			broadcastMessage(conn, name+": "+message)
+		}
+	}
+}
+
+// broadcastMessage sends a message to all connected clients except the sender
+func broadcastMessage(sender net.Conn, message string) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	for client := range clients {
+		if client != sender {
+			client.Write([]byte(message + "\n"))
+		}
+	}
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":8989")
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+		return
+	}
+	defer listener.Close()
+
+	fmt.Println("Server is listening on port 8989...")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+
+		go handleConnection(conn)
+	}
+}
