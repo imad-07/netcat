793f52f71cbdb83d91744b0c3190ca9c43feee3f
Imad Marwan Tounsadi <tounsadiimad@gmail.com>
2024-09-10 13:30:34 +0100

    heyy

--- main.go
+++ main.go
@@ -41,8 +41,8 @@ func handleConnection(conn net.Conn) {
 	name := ""
 	var err error
 	count := 0
-	for len(name) == 0 && len(clients) <= 2 {
-		if len(clients) == 2 {
+	for len(name) == 0 && len(clients) <= 10 || isArrowKey(name){
+		if len(clients) == 10 {
 			conn.Write([]byte("Max clients reached\n"))
 			return
 		}
@@ -51,7 +51,7 @@ func handleConnection(conn net.Conn) {
 		conn.Write([]byte("[ENTER YOUR NAME]:"))
 		name, err = reader.ReadString('\n')
 		name = strings.TrimSpace(name)
-		if err != nil || len(name) == 0 {
+		if err != nil || len(name) == 0 || isArrowKey(name){
 			conn.Write([]byte("Error reading name\n"))
 			fmt.Println("Error reading name")
 			count++
@@ -59,13 +59,14 @@ func handleConnection(conn net.Conn) {
 		}
 		if count == 3 {
 			conn.Write([]byte("you ran out of tries\n"))
+			count = 0
 			return
 		}
 
 	}
 
 	fmt.Println(" has joined our chat...", name)
-	broadcastMessage(conn, name+" has joined our chat...")
+	broadcastMessage(conn, "\n"+name+" has joined our chat...")
 
 	mu.Lock()
 	clients[conn] = name // Add client to the map
@@ -77,15 +78,19 @@ func handleConnection(conn net.Conn) {
 	if err != nil {
 		return
 	}
-
 	conn.Write([]byte(str))
+	broadcasttyping()
 	for {
-		// date := time.Now().Format("2006-01-02 15:04:05")
+		date := time.Now().Format("2006-01-02 15:04:05")
 
 		message, err := reader.ReadString('\n')
-		if err != nil {
+		if isArrowKey(message){
+			conn.Write([]byte("u cannot write anarrow my freind :)\n"))
+			message=""
+		}else if err != nil {
+			broadcastMessage(conn, "\n"+name+" has left our chat...")
 			fmt.Println(name + " has left our chat...")
-			broadcastMessage(conn, name+" has left our chat...")
+			broadcasttyping()
 			mu.Lock()
 			delete(clients, conn)
 			mu.Unlock()
@@ -96,8 +101,10 @@ func handleConnection(conn net.Conn) {
 		if len(message) != 0 {
 			// Broadcast the message to all clients
 			broadcastMessage(conn, "["+name+"]"+": "+message)
+			broadcasttyping()
+		} else {
+			conn.Write([]byte("[" + date + "] " + "[" + name + "]:"))
 		}
-		broadcasttyping()
 
 	}
 }
@@ -109,8 +116,12 @@ func broadcastMessage(sender net.Conn, message string) {
 	mu.Lock()
 	defer mu.Unlock()
 	for client := range clients {
-		if client != sender {
-			client.Write([]byte("\n[" + date + "]" + " " + message + "\n"))
+		if client != sender && message != "\n" {
+			if strings.HasSuffix(message, "has joined our chat...") || strings.HasSuffix(message, "has left our chat...") {
+				client.Write([]byte("\n" + message[1:] + "\n"))
+			} else {
+				client.Write([]byte("\n[" + date + "]" + " " + message[1:] + "\n"))
+			}
 		}
 	}
 
@@ -125,6 +136,7 @@ func broadcastMessage(sender net.Conn, message string) {
 	if !(strings.HasSuffix(message, "has joined our chat...") || strings.HasSuffix(message, "has left our chat...")) {
 		_, err = file.WriteString("[" + date + "] " + message + "\n")
 	}
+	message = ""
 	if err != nil {
 		fmt.Println("Error writing to file:", err)
 	}
@@ -149,7 +161,7 @@ func main() {
 		return
 	}
 	defer listener.Close()
-	fmt.Printf("Server is listening on port %s...\n",port)
+	fmt.Printf("Server is listening on port %s...\n", port)
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
@@ -159,4 +171,18 @@ func main() {
 
 		go handleConnection(conn)
 	}
+}
+func isArrowKey(s string) bool {
+	arrowKeys := []string{
+		"\x1b[A", // Up arrow
+		"\x1b[B", // Down arrow
+		"\x1b[C", // Right arrow
+		"\x1b[D", // Left arrow
+	}
+	for _, key := range arrowKeys {
+		if strings.Contains(s, key) {
+			return true
+		}
+	}
+	return false
 }
